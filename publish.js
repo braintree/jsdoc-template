fa0a1c68a723a1696f24bffc5b50c67373843bd6
Tony Mobily <merc@mobily1.com>
2018-01-31 09:30:10 +0800

    Enable extra entries in side bar

--- publish.js
+++ publish.js
@@ -356,11 +356,11 @@ function buildNav(members) {
   nav += buildMemberNav(members.modules, 'Modules', {}, linkto);
     // TODO: as needed, comment back in later
     // nav += buildMemberNav(members.externals, 'Externals', seen, linktoExternal);
-    // nav += buildMemberNav(members.events, 'Events', seen, linkto);
-    // nav += buildMemberNav(members.namespaces, 'Namespaces', seen, linkto);
-    // nav += buildMemberNav(members.mixins, 'Mixins', seen, linkto);
+   nav += buildMemberNav(members.events, 'Events', seen, linkto);
+   nav += buildMemberNav(members.namespaces, 'Namespaces', seen, linkto);
+   nav += buildMemberNav(members.mixins, 'Mixins', seen, linkto);
     // nav += buildMemberNav(members.tutorials, 'Tutorials', seenTutorials, linktoTutorial);
-    // nav += buildMemberNav(members.interfaces, 'Interfaces', seen, linkto);
+   nav += buildMemberNav(members.interfaces, 'Interfaces', seen, linkto);
 
   if (members.globals.length) {
     members.globals.forEach(function (g) {
